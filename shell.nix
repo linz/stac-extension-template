f7cc3f2d5d29e6c079f33bd6091d60ea7947dcd4
Victor Engmark <vengmark@linz.govt.nz>
2024-06-10 11:00:59 +1200

    refactor: Format Nix file using nixfmt-rfc-style

--- shell.nix
+++ shell.nix
@@ -1,15 +1,8 @@
-{ pkgs ? import
-    (
-      fetchTarball
-        {
-          name = "nixos-unstable-2024-06-05";
-          url = "https://github.com/nixos/nixpkgs/archive/57610d2f8f0937f39dbd72251e9614b1561942d8.tar.gz";
-          sha256 = "0k8az8vmfdk1n8xlza252sqk0hm1hfc7g67adin6jxqaab2s34n9";
-        })
-    { }
+{
+  pkgs ? import (fetchTarball {
+    name = "nixos-unstable-2024-06-05";
+    url = "https://github.com/nixos/nixpkgs/archive/57610d2f8f0937f39dbd72251e9614b1561942d8.tar.gz";
+    sha256 = "0k8az8vmfdk1n8xlza252sqk0hm1hfc7g67adin6jxqaab2s34n9";
+  }) { },
 }:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.nodejs
-  ];
-}
+pkgs.mkShell { buildInputs = [ pkgs.nodejs ]; }
